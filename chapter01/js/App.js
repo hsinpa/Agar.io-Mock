f3c838dcb90114a317dbb1ce834fea699fd181f0
HsiN <hsinpa@gmail.com>
2015-07-04 16:28:50 +0800

    finish
    finisih

--- chapter01/js/App.js
+++ chapter01/js/App.js
@@ -1,30 +1,38 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO),
-    graphics,
+//建立遊戲
+var game = new Phaser.Game(600, 400, Phaser.AUTO),
     player,
     speed = 5;
 
-
+//初始遊戲State, 就是你進去遊戲第一個會看到的東西, 放在Phaser.AUTO後面也行
 var GameState =  {
   preload : function() {
+    //load background先
     game.load.image('background','images/background.png');
   },
 
   create : function(){
+    //設定世界大小, 然後random出玩家的位子
+    var spawnXPos = Math.floor((Math.random() * 1920) + 1),
+        spawnYPos = Math.floor((Math.random() * 1920) + 1);
+
         game.add.tileSprite(0, 0, 1920, 1920, 'background');
         game.world.setBounds(0, 0, 1920, 1920);
-        player = new Player(game);
-        player.set();
+        //玩家
+        player = new Player(game, spawnXPos, spawnYPos);
   },
 
   update : function() {
-      //player.graphics.clear();
-      player.move();
+      //60fps, 刷新玩家位子
+      player.set();
 
+      //滑鼠位子 - 玩家位子 得出雙方差距然後normalized, 將最後的數值加到play position上
+      //玩家就會順著滑鼠方向移動
       var xDis = (game.input.x + game.camera.x)- (player.graphics.position.x ),
           yDis = (game.input.y + game.camera.y) - (player.graphics.position.y),
           normalized = Math.sqrt((xDis * xDis) + (yDis * yDis));
           moveDir = {x : xDis / normalized,
                          y : yDis / normalized };
+
           //console.log(player.position.x + ", " + player.position.y);
           // if (player.position.y < game.world.height && player.position.y > 0 &&
           //     player.position.x < game.world.width && player.position.x > 0) {
@@ -36,9 +44,12 @@ var GameState =  {
 
           player.position.y = player.position.y + moveDir.y * speed;
           player.position.x = player.position.x + moveDir.x * speed;
+
+          //設定攝影機永遠跟著玩家
           game.camera.follow(player.graphics);
   }
 };
 
+//加入以上state, 然後啟動
 game.state.add("GameState", GameState);
 game.state.start("GameState");
--- chapter01/js/Game/Player/PlayerManager.js
+++ chapter01/js/Game/Player/PlayerManager.js
@@ -1,24 +1,18 @@
-function Player( game) {
-    this.position = {x : 100, y : 100, r : 50 };
+function Player( game, x, y) {
+    this.position = {x : x, y : y, r : 20 };
     this.graphics = game.add.graphics(this.position.x, this.position.y);
 }
 
 Player.prototype.set = function() {
+    //做個圓圈
     this.graphics.lineStyle(0);
     this.graphics.beginFill(0xFFFF0B, 0.5);
     this.graphics.drawCircle(0, 0, this.position.r);
     this.graphics.endFill();
-
+    //刷新位子
     this.graphics.x = this.position.x;
     this.graphics.y = this.position.y;
 }
-
-
-Player.prototype.move = function() {
-    this.graphics.x = this.position.x;
-    this.graphics.y = this.position.y;
-}
-
 
 Player.prototype.demoObject = function(x, y) {
   this.graphics.lineStyle(0);
